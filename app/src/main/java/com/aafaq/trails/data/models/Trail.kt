c94d22e6b560a3c924aff31ff6df57a27b0375ae
Ali <aafaq@msn.com>
2023-07-18 00:14:18 +0500

    Added data classes

--- app/src/main/java/com/aafaq/trails/data/models/Trail.kt
+++ app/src/main/java/com/aafaq/trails/data/models/Trail.kt
@@ -1,3 +1,23 @@
 package com.aafaq.trails.data.models
 
-data class Trail(val id: Int? = null, val tag: String? = null, val debugMessage: String? = null)
+import com.aafaq.trails.utils.Constants
+
+data class Trail(
+    val id: Int? = null,
+    val tag: String? = null,
+)
+
+data class TrailLogDetails(
+    /**
+     * for debug
+     * */
+    var debugTag: String = Constants.DEFAULT_TRAIL_TAG,
+    val debugMessage: String? = null,
+
+    var title: String? = null,
+    var description: String? = null
+){
+    fun asString(): String{
+        return "$debugTag --> $title: $description\n"
+    }
+}
---
+++ app/src/main/java/com/aafaq/trails/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.aafaq.trails.utils
+
+object Constants {
+    const val DEFAULT_TRAIL_TAG = "trailing_log"
+}
